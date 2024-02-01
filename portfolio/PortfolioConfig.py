e77f7ae5ce28b57b822836d1b265cd79191c3842
yeoshuheng <yeoshuheng@gmail.com>
2024-02-01 21:51:16 +0800

    feat: add ability to use non-downloaded data using wdrs

--- portfolio/PortfolioConfig.py
+++ portfolio/PortfolioConfig.py
@@ -1,5 +1,7 @@
 import pandas as pd
-import math
+import math, os
+import wrds
+import refinitiv.data
 
 """
 This class represents a portfolio configuration, the portfolio configuration is defined by
@@ -20,18 +22,32 @@ class InvalidRatioException(Exception):
 
     def __init__(self, ratio):
         super().__init__("The sum of your current portfolio weights is {}! Check your ratios again.".format(ratio))
+        
+class DataPullException(Exception):
+    def __init__(self):
+        super().__init__("Problem with retrieving data, check your ticker symbol against WRDS data dictionary!")
 
 class PortfolioConfig:
-
-    def __init__(self, name : str, holdings : dict):
+    
+    """
+    @param name The name of the portfolio.
+    @param holdings The dictionary of holdings, consisting of ticker and weights.
+    @param download True if we have all the data in ./data, if some of our tickers are not in ./data, we pull from WRDS.
+    """
+    def __init__(self, name : str, holdings : dict, download=False):
         self.name = name
         self.holdings = holdings
+        self.download = download
         self.validate_holdings_ratio()
+        self.path = "../data/"
+        if not self.download:
+            self.downloaded = {}
+            self.get_data()
     
     """
     Ensure portfolio weights are valid.
     """
-    def validate_holdings_ratio(self):
+    def validate_holdings_ratio(self) -> None:
         sum_ = 0.0
         for asset, ratios in self.holdings.items():
             if ratios > 1 or ratios < 0:
@@ -39,3 +55,27 @@ class PortfolioConfig:
             sum_ += float(ratios)
         if not math.isclose(sum_, 1.0):
             raise InvalidRatioException(sum_)
+            
+    """
+    Gets data needed from WRDS.
+    """
+    def get_data(self) -> None:
+        conn = wrds.Connection()
+        for asset in self.holdings.keys():
+            path = os.path.join(self.path, "{}.csv".format(asset))
+            if os.path.isfile(path):
+                continue
+            try:
+                asset_data = conn.raw_sql("""
+                select datadate, conm, tic, prccd
+                from comp_na_daily_all.secd
+                where tic = '{}'
+                """.format(asset))
+            except:
+                raise DataPullException()
+            if self.download:
+                asset_data.to_csv(path)
+            else: # We don't download to ./data, but keep pulled data in memory.
+                self.downloaded[asset] = asset_data
+                
+    
